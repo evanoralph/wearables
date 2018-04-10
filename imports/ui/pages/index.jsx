20d9073c71c751b6c06ee04d7b6f3442ac5f3803
daryllm <daryllm@sonarlogic.biz>
2018-04-10 14:58:32 +0800

    Added main index page

--- imports/ui/pages/index.jsx
+++ imports/ui/pages/index.jsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import { Link } from 'react-router-dom';
 
 class Index extends React.Component {
 
   render() {
     return(
-    <div className="Index">
-      <h2>Base</h2>
-      <p>A starting point for Meteor applications.</p>
-      <p><a className="btn btn-success" href="https://themeteorchef.com/base" role="button">Read the Documentation</a>
-      </p>
-      <p style={ {fontSize: '16px', color: '#aaa'} }>Currently at v4.15.0</p>
+    <div id="index-page" className="Index">
+      <div className="logo"></div>
+      <div className="col-xs-12 no-padding sm-button">
+        <Link to="/sign-up-selection"><div className="row button middle-xs center-xs">SIGN UP</div></Link>
+      </div>
+      <div className="col-xs-12 no-padding sm-button">
+        <Link to="/login"><div className="row button middle-xs center-xs">LOG IN</div></Link>
+      </div>
     </div>
     )
   }
