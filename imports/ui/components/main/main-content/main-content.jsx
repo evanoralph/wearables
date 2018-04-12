1d7febc788618852781f8efa19a6977e4c16ca9d
daryllm <daryllm@sonarlogic.biz>
2018-04-12 10:17:58 +0800

    Added Jewelry Box Collection, layout fixes

--- imports/ui/components/main/main-content/main-content.jsx
+++ imports/ui/components/main/main-content/main-content.jsx
@@ -6,12 +6,41 @@ import HeaderNav from '../../../components/header/header-nav/header-nav-containe
 
 class MainContent extends React.Component {
 
+  editJewelry() {
+    alert("This will edit the current jewelry");
+  }
+
+  addJewelry() {
+    alert("This will add a new jewelry");
+  }
+
   render() {
     return(
-      <div id="main-page" className="row middle-xs no-margin">
+      <div id="main-content" className="content-container no-margin">
         <div className="col-xs-12 no-padding">
-          MAIN CONTENT
+          <span className="content-title">JEWELRY COLLECTION</span>
         </div>
+
+        <div id="jewelry-collection">
+
+          <div className="jewelry-box has-item">
+            <div className="edit-jewelry" onClick={this.editJewelry.bind(this)}></div>
+            <div className="jewelry-image" style={{backgroundImage: `url(${'url-of-image-here'})`}}></div>
+            <div className="jewelry-details">
+              <span className="jewelry-type">Bracelet</span>
+              <span className="jewelry-name">Carnelian Brass</span>
+              <span className="jewelry-series">Fall 2016</span>
+            </div>
+          </div>
+          <div className="jewelry-box add-item" onClick={this.addJewelry.bind(this)}>
+          </div>
+          <div className="jewelry-box">
+          </div>
+          <div className="jewelry-box">
+          </div>
+
+        </div>
+
       </div>
     )
   }
