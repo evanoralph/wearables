0104ad0df0cadfa2d2c9c267073886d4cade1508
Daryll <dsmalicsi@gmail.com>
2018-05-04 16:47:17 +0800

    Added Notifications Listener (Android Only)

--- client/main.js
+++ client/main.js
@@ -3,3 +3,21 @@
 import 'flexboxgrid';
 import '/imports/startup/client';
 import '/imports/startup/both';
+
+Meteor.startup(function() {
+    console.log("startup")
+    if (Meteor.isCordova) {
+        alert("isCordova")
+        console.log("isCordova")
+        document.addEventListener('deviceready', function() {
+            notificationListener.listen(function(n){
+                console.log("Received notification " + JSON.stringify(n) );
+                alert("Received notification " + JSON.stringify(n) );
+            }, function(e){
+                console.log("Notification Error " + e);
+                alert("Notification Error " + e);
+            });
+        })
+
+    }
+})
