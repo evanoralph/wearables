ee5a936f9ca4d04772f7da0dea26dfac3a943708
Daryll <dsmalicsi@gmail.com>
2018-05-18 14:08:03 +0800

    Fixed Social Media Button Layout

--- imports/ui/components/imports/imports-buttons/imports-buttons.jsx
+++ imports/ui/components/imports/imports-buttons/imports-buttons.jsx
@@ -39,11 +39,11 @@ class ImportButtons extends React.Component {
         <div className="instructions">Click one of the apps below to proceed.</div>
         <div className="buttons-container">
             <div className="button-grid"><div className="button-item android" id="phoneImport" onClick={this.openImportDialog.bind(this, "PHONE")}></div></div>
+            <div className="button-grid"><div className="button-item" id="whatsappImport" onClick={this.openImportDialog.bind(this, "WHATSAPP")}></div></div>
             <div className="button-grid"><div className="button-item" id="facebookImport" onClick={this.openImportDialog.bind(this, "FACEBOOK")}></div></div>
             <div className="button-grid"><div className="button-item" id="googleImport" onClick={this.openImportDialog.bind(this, "GOOGLE+")}></div></div>
-            <div className="button-grid"><div className="button-item" id="whatsappImport" onClick={this.openImportDialog.bind(this, "WHATSAPP")}></div></div>
-            <div className="button-grid"><div className="button-item" id="linkedinImport" onClick={this.openImportDialog.bind(this, "LINKEDIN")}></div></div>
             <div className="button-grid"><div className="button-item" id="twitterImport" onClick={this.openImportDialog.bind(this, "TWITTER")}></div></div>
+            <div className="button-grid"><div className="button-item" id="linkedinImport" onClick={this.openImportDialog.bind(this, "LINKEDIN")}></div></div>
         </div>
         <div className="reminder">
           Need to import contacts from at least one app to finish setup.
