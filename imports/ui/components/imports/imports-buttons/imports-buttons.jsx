6d9ac2c072cac9e661fcad960300c6546538eb76
Daryll <dsmalicsi@gmail.com>
2018-05-18 10:06:08 +0800

    Added Social Media icons, polished styling

--- imports/ui/components/imports/imports-buttons/imports-buttons.jsx
+++ imports/ui/components/imports/imports-buttons/imports-buttons.jsx
@@ -36,17 +36,17 @@ class ImportButtons extends React.Component {
   render() {
     return(
       <div id="import-buttons" className="row no-padding no-margin" >
-        <div className="instructions">Click one of the apps below to proceed</div>
+        <div className="instructions">Click one of the apps below to proceed.</div>
         <div className="buttons-container">
-          <div className="button-item" id="phoneImport" onClick={this.openImportDialog.bind(this, "PHONE")}></div>
-          <div className="button-item" id="facebookImport" onClick={this.openImportDialog.bind(this, "FACEBOOK")}></div>
-          <div className="button-item" id="googleImport" onClick={this.openImportDialog.bind(this, "GOOGLE+")}></div>
-          <div className="button-item" id="whatsappImport" onClick={this.openImportDialog.bind(this, "WHATSAPP")}></div>
-          <div className="button-item" id="linkedinImport" onClick={this.openImportDialog.bind(this, "LINKEDIN")}></div>
-          <div className="button-item" id="twitterImport" onClick={this.openImportDialog.bind(this, "TWITTER")}></div>
+            <div className="button-grid"><div className="button-item android" id="phoneImport" onClick={this.openImportDialog.bind(this, "PHONE")}></div></div>
+            <div className="button-grid"><div className="button-item" id="facebookImport" onClick={this.openImportDialog.bind(this, "FACEBOOK")}></div></div>
+            <div className="button-grid"><div className="button-item" id="googleImport" onClick={this.openImportDialog.bind(this, "GOOGLE+")}></div></div>
+            <div className="button-grid"><div className="button-item" id="whatsappImport" onClick={this.openImportDialog.bind(this, "WHATSAPP")}></div></div>
+            <div className="button-grid"><div className="button-item" id="linkedinImport" onClick={this.openImportDialog.bind(this, "LINKEDIN")}></div></div>
+            <div className="button-grid"><div className="button-item" id="twitterImport" onClick={this.openImportDialog.bind(this, "TWITTER")}></div></div>
         </div>
         <div className="reminder">
-          Need to import contacts from at least one app to finish setup
+          Need to import contacts from at least one app to finish setup.
         </div>
       </div>
     ) ;
