226492bc94a5f8c75954030bcc390643ba712826
ralphevano <evanoralph@gmail.com>
2018-06-14 23:01:58 +0800

    fix

--- imports/ui/components/sign-up/actions.js
+++ imports/ui/components/sign-up/actions.js
@@ -27,44 +27,60 @@ export default {
       }
     })
   },
-  loginWithFacebook(){
-    Meteor.linkWithFacebook({
+  loginWithFacebook({Meteor},history){
+    Meteor.loginWithFacebook({
       loginStyle: "popup" ,
-      requestPermissions: ['email']
+      requestPermissions: ['email','user_friends']
     }, function (err) {
       if (err) {
         swal("error when login with facebook " + err);
         return;
       }
+      history.push("/main")
     });
+    // Meteor.loginWithTwitter({
+    //   requestPermissions: ['basic'] // cu
+    // }, function (err) {
+    //   if (err) {
+    //     swal("error when login with facebook " + err);
+    //     return;
+    //   }
+    //   history.push("/main")
+    // });
   },
   userLoginGoogle({Meteor},history){
 
-    if (Meteor.isCordova) {
-
-      Meteor.linkWithGoogle({
+    if (Meteor.isCordova) { // signIn through cordova
+      Meteor.loginWithGoogle({
         loginStyle: "popup" ,
         'webClientId': 'com.googleusercontent.apps.825480306969-uglck4esst2m4urn33fl92qb5mjkbiih',
-        requestPermissions: ['email'],
-        loginUrlParameters: {include_granted_scopes: true},
+        loginUrlParameters: {include_granted_scopes: true,scope:'https://www.googleapis.com/auth/contacts.readonly'},
         requestOfflineToken: true,
         requestPermissions: ['email', 'profile', 'https://www.googleapis.com/auth/contacts.readonly'],
       }, (error) => {
-        if (error) swal({text:error});
-        
-        history.push("/main")
+        if (error) alert(error);
+
+        history.push("/main");
       });
+      // Meteor.cordova_g_plus({
+      //   cordova_g_plus: true,
+      //   profile: ["email", "email_verified", "gender", "locale", "name", "picture"],
+      //   webClientId: '825480306969-uglck4esst2m4urn33fl92qb5mjkbiih.apps.googleusercontent.com',
+      // }, (error) => {
+      //   if (error) {
+      //     // error handling code
+      //   }
+      // });
     } else { // signIn through browser
       if (Accounts.loginServicesConfigured()) {
-        Meteor.linkWithGoogle({
+        Meteor.loginWithGoogle({
           loginStyle: "popup" ,
           'webClientId': 'com.googleusercontent.apps.825480306969-uglck4esst2m4urn33fl92qb5mjkbiih',
-          requestPermissions: ['email'],
           loginUrlParameters: {include_granted_scopes: true},
           requestOfflineToken: true,
           requestPermissions: ['email', 'profile', 'https://www.googleapis.com/auth/contacts.readonly'],
         }, (error) => {
-          if (error) swal({text:error});
+          if (error) alert(error);
         });
       }
     }
